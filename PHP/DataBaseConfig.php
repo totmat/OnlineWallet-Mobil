cfd9dcf8f89f1f79f42b09ea4428fab671a07685
sz0totmat <sz0totmat@szt1-16-hp>
2022-04-04 08:06:29 +0200

    Php javítás

---
+++ PHP/DataBaseConfig.php
@@ -0,0 +1,16 @@
+<?php
+class DataBaseConfig
+{
+	public $servername;
+	public $username;
+	public $password;
+	public $databasename;
+	
+	public function __construct(){
+		$this->servername = 'localhost';
+		$this->username = 'root';
+		$this->password = '';
+		$this->databasename = 'onlinewallet';
+	}
+}
+?>
